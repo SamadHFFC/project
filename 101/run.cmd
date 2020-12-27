4bf83f57bf7da7bb8191a0b8bd1e845f03c740cd
Samad Ali <nabizadasamadali@gmail.com>
2020-12-27 22:48:07 +0530

    my first maven commit

---
+++ 101/run.cmd
@@ -0,0 +1,1 @@
+javadoc -docletpath target\101-1.0-SNAPSHOT-jar-with-dependencies.jar -doclet 20.Doclet %*
---
+++ 101/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javadoc -docletpath target/101-1.0-SNAPSHOT-jar-with-dependencies.jar -doclet 20.Doclet "$@"
